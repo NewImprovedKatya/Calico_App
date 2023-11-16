d2fe3a10711831a46d7c2b4e736d5320f933038d
NewImprovedKatya <135748170+NewImprovedKatya@users.noreply.github.com>
2023-11-15 18:32:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,60 @@
 const catList = {
-    redFemale: {
-      chroma: ["red", "red"],
-      patches: "none",
-    },
-    blackFemale: {
-      chroma: ["black", "black"],
-      patches: "none",
-    },
-    redMale: {
-      chroma: ["red", "Y"],
-      patches: "none",
-    },
-    blackMale: {
-      chroma: ["black", "Y"],
-      patches: "none",
-    },
-    redFemalePatches: {
-      chroma: ["red", "red"],
-      patches: "white",
-    },
-    blackFemalePatches: {
-      chroma: ["black", "black"],
-      patches: "white",
-    },
-    redMalePatches: {
-      chroma: ["red", "Y"],
-      patches: "white",
-    },
-    blackMalePatches: {
-      chroma: ["black", "Y"],
-      patches: "white",
-    },
-    tortie: {
-      chroma: ["black", "red"],
-      patches: "none",
-    },
-    calico: {
-      chroma: ["black", "red"],
-      patches: "white",
-    },
-  };
-  
+  redFemale: {
+    chroma: ["red", "red"],
+    patches: "none",
+  },
+  blackFemale: {
+    chroma: ["black", "black"],
+    patches: "none",
+  },
+  redMale: {
+    chroma: ["red", "Y"],
+    patches: "none",
+  },
+  blackMale: {
+    chroma: ["black", "Y"],
+    patches: "none",
+  },
+  redFemalePatches: {
+    chroma: ["red", "red"],
+    patches: "white",
+  },
+  blackFemalePatches: {
+    chroma: ["black", "black"],
+    patches: "white",
+  },
+  redMalePatches: {
+    chroma: ["red", "Y"],
+    patches: "white",
+  },
+  blackMalePatches: {
+    chroma: ["black", "Y"],
+    patches: "white",
+  },
+  tortie: {
+    chroma: ["black", "red"],
+    patches: "none",
+  },
+  calico: {
+    chroma: ["black", "red"],
+    patches: "white",
+  },
+};
 
-  const radioButtons = document.querySelectorAll('input[name="cat"]');
-  const secondradioButtons = document.querySelectorAll('input[name="male"]');
-  const thirdButtons = document.querySelectorAll('input[name="motherTwo"]');
-  const fourthButtons = document.querySelectorAll('input[name="maleTwo"]');
-  
 
-  const testerSpan = document.querySelector('#tester');
-  
-  const submitButton = document.querySelector("#tortieButton");
-  const secondsubmitButton = document.querySelector("#calicoButton");
+alert("working");
+
+
+const radioButtons = document.querySelectorAll('input[name="cat"]');
+const secondRadioButtons = document.querySelectorAll('input[name="male"]');
+const thirdButtons = document.querySelectorAll('input[name="motherTwo"]');
+const fourthButtons = document.querySelectorAll('input[name="maleTwo"]');
+
+
+const testerSpan = document.querySelector('#tester');
+
+const submitButton = document.querySelector("#tortieButton");
+const secondSubmitButton = document.querySelector("#calicoButton");
 
 const firstKitten = document.querySelector("#firstKitten");
 const secondKitten = document.querySelector("#secondKitten");
@@ -61,33 +64,33 @@ const fourthKitten = document.querySelector("#fourthKitten");
 
 
 
-  submitButton.addEventListener('click', () => {
-  
-    const selectedCat = document.querySelector('input[name="cat"]:checked').value;
-    const secondselectedCat = document.querySelector('input[name="male"]:checked').value;
-  
+submitButton.addEventListener('click', () => {
+
+  const selectedCat = document.querySelector('input[name="cat"]:checked').value;
+  const secondSelectedCat = document.querySelector('input[name="male"]:checked').value;
+
 let results= [];
 
 for (let i = 0; i < 4; i++) {
 
 
-    const kitten = {
-      chroma: [catList[selectedCat].chroma[Math.round(Math.random()*1)], 
-      catList[secondselectedCat].chroma[Math.round(Math.random()*1)]]
-    };
-  
- 
-    if (kitten.chroma.includes("red") && kitten.chroma.includes("black")) {
-      results.push(" tortoiseshell female");
-    } else if (kitten.chroma.includes("red") && kitten.chroma.includes("Y")) {
-      results.push("red male");
-    } else if (kitten.chroma.includes("black") && kitten.chroma.includes("Y")) {
-      results.push("black male");
-    } else if (kitten.chroma.includes("red")) {
-      results.push("red female");
-    } else {
-      results.push("black female");
-    };
+  const kitten = {
+    chroma: [catList[selectedCat].chroma[Math.round(Math.random()*1)], 
+    catList[secondSelectedCat].chroma[Math.round(Math.random()*1)]]
+  };
+
+
+  if (kitten.chroma.includes("red") && kitten.chroma.includes("black")) {
+    results.push("-265px -1825px");
+  } else if (kitten.chroma.includes("red") && kitten.chroma.includes("Y")) {
+    results.push("-5px -1825px");
+  } else if (kitten.chroma.includes("black") && kitten.chroma.includes("Y")) {
+    results.push("-135px -1825px");
+  } else if (kitten.chroma.includes("red")) {
+    results.push("-5px -1825px");
+  } else {
+    results.push("-135px -1825px");
+  };
 
 };
 
@@ -95,48 +98,70 @@ firstKitten.textContent = results[0];
 secondKitten.textContent = results[1];
 thirdKitten.textContent = results[2];
 fourthKitten.textContent = results[3];
-  });
 
 
-  
+document.getElementById("kitten1").style.backgroundPosition = results[0];
 
-  secondsubmitButton.addEventListener('click', () => {
+document.getElementById("kitten2").style.backgroundPosition = results[1];
 
-    const selectedMother = document.querySelector('input[name="motherTwo"]:checked').value;
-    const selectedFather = document.querySelector('input[name="maleTwo"]:checked').value;
-  
-    const kittenOnePatches = [catList[selectedMother].patches, catList[selectedFather].patches];
-    const kittenOne = {
-      chroma: [catList[selectedMother].chroma[Math.round(Math.random()*1)], 
-        catList[selectedFather].chroma[Math.round(Math.random()*1)]], 
-      patches: kittenOnePatches[Math.round(Math.random()*1)]
+document.getElementById("kitten3").style.backgroundPosition = results[2];
+
+document.getElementById("kitten4").style.backgroundPosition = results[3];
+
+});
+
+
+
+
+secondSubmitButton.addEventListener('click', () => {
+
+let coatOneColor= [];
+
+for (let i = 0; i < 4; i++) {
+
+  const selectedMother = document.querySelector('input[name="motherTwo"]:checked').value;
+  const selectedFather = document.querySelector('input[name="maleTwo"]:checked').value;
+
+  const kittenOnePatches = [catList[selectedMother].patches, catList[selectedFather].patches];
+  const kittenOne = {
+    chroma: [catList[selectedMother].chroma[Math.round(Math.random()*1)], 
+      catList[selectedFather].chroma[Math.round(Math.random()*1)]], 
+    patches: kittenOnePatches[Math.round(Math.random()*1)]
+  };
+
+    if (kittenOne.chroma.includes("red") && kittenOne.chroma.includes("black") && kittenOne.patches === "white") {
+        coatOneColor.push("-265px -2025px");
+    } else if (kittenOne.chroma.includes("red") && kittenOne.chroma.includes("black")) {
+      coatOneColor.push("-265px -2025px");
+    } else if (kittenOne.chroma.includes("red") && kittenOne.chroma.includes("Y") && kittenOne.patches === "white") {
+      coatOneColor.push("-5px -2025px");
+    } else if (kittenOne.chroma.includes("black") && kittenOne.chroma.includes("Y") && kittenOne.patches === "white") {
+      coatOneColor.push("-135px -2025px");
+    } else if (kittenOne.chroma.includes("red") && kittenOne.patches === "white") {
+        coatOneColor.push("-5px -2025px");
+    } else if (kittenOne.chroma.includes("black") && kittenOne.patches === "white") {
+        coatOneColor.push("-135px -2025px");
+    } else if (kittenOne.chroma.includes("red") && kittenOne.chroma.includes("Y")) {
+      coatOneColor.push("-5px -1825px");
+    } else if (kittenOne.chroma.includes("black") && kittenOne.chroma.includes("Y")) {
+      coatOneColor.push("-135px -1825px");
+    } else if (kittenOne.chroma.includes("red")) {
+      coatOneColor.push("-5px -1825px");
+    } else {
+     coatOneColor.push("-135px -1825px");
     };
-
-
+};
   
-    let coatOneColor;
-      if (kittenOne.chroma.includes("red") && kittenOne.chroma.includes("black") && kittenOne.patches === "white") {
-          coatOneColor = "female calico";
-      } else if (kittenOne.chroma.includes("red") && kittenOne.chroma.includes("black")) {
-        coatOneColor = "female tortoiseshell";
-      } else if (kittenOne.chroma.includes("red") && kittenOne.chroma.includes("Y") && kittenOne.patches === "white") {
-        coatOneColor = "red and white Male";
-      } else if (kittenOne.chroma.includes("black") && kittenOne.chroma.includes("Y") && kittenOne.patches === "white") {
-        coatOneColor = "black and white male";
-      } else if (kittenOne.chroma.includes("red") && kittenOne.patches === "white") {
-          coatOneColor = "female red and white";
-      } else if (kittenOne.chroma.includes("black") && kittenOne.patches === "white") {
-          coatOneColor = "female black and white";
-      } else if (kittenOne.chroma.includes("red") && kittenOne.chroma.includes("Y")) {
-        coatOneColor = "male red";
-      } else if (kittenOne.chroma.includes("black") && kittenOne.chroma.includes("Y")) {
-        coatOneColor = "black male";
-      } else if (kittenOne.chroma.includes("red")) {
-        coatOneColor= "red female";
-      } else {
-        coatOneColor= "black female";
-      };
-    
-      testerSpan.textContent = coatOneColor;
-    
-  });
+    testerSpan.textContent = coatOneColor;
+
+document.getElementById("kitten5").style.backgroundPosition = coatOneColor[0];
+
+document.getElementById("kitten6").style.backgroundPosition = coatOneColor[1];
+
+document.getElementById("kitten7").style.backgroundPosition = coatOneColor[2];
+
+document.getElementById("kitten8").style.backgroundPosition = coatOneColor[3];
+
+
+
+});
